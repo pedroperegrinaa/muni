c5f623c1427252f06e1e50364de149eb4519ab56
Rushmerk <57403682+pedroperegrinaa@users.noreply.github.com>
2022-01-19 01:53:13 -0300

    Isso vai dar trabalho dms

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
